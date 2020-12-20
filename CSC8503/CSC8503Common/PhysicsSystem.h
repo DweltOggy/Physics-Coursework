92ea05604897f0baaef20abb6b0445b145db02b0
Chris Oghoetuoma <c0062213@newcastle.ac.uk>
2020-12-20 13:24:41 +0000

    Add project files.

---
+++ CSC8503/CSC8503Common/PhysicsSystem.h
@@ -0,0 +1,65 @@
+#pragma once
+#include "../CSC8503Common/GameWorld.h"
+#include <set>
+
+namespace NCL {
+	namespace CSC8503 {
+		class PhysicsSystem	{
+		public:
+			PhysicsSystem(GameWorld& g);
+			~PhysicsSystem();
+
+			void Clear();
+
+			void Update(float dt);
+
+			void UseGravity(bool state) {
+				applyGravity = state;
+			}
+
+			void SetGlobalDamping(float d) {
+				globalDamping = d;
+			}
+
+			void SetLinearDamping(float d) {
+				linearDamping = d;
+			}
+
+			float getLinearDamping()
+			{
+				return linearDamping;
+			}
+			void SetGravity(const Vector3& g);
+		protected:
+			void BasicCollisionDetection();
+			void BroadPhase();
+			void NarrowPhase();
+
+			void ClearForces();
+
+			void IntegrateAccel(float dt);
+			void IntegrateVelocity(float dt);
+
+			void UpdateConstraints(float dt);
+
+			void UpdateCollisionList();
+			void UpdateObjectAABBs();
+
+			void ImpulseResolveCollision(GameObject& a , GameObject&b, CollisionDetection::ContactPoint& p) const;
+
+			GameWorld& gameWorld;
+
+			bool	applyGravity;
+			Vector3 gravity;
+			float	dTOffset;
+			float	globalDamping;
+			float	linearDamping;
+
+			std::set<CollisionDetection::CollisionInfo> allCollisions;
+
+			bool useBroadPhase		= true;
+			int numCollisionFrames	= 5;
+		};
+	}
+}
+
